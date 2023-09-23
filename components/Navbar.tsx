b6b30c5f44e8cb3e7b161d2992f78b1c49ca775d
Abdulrahman Nahhas <nahhas909@gmail.com>
2023-09-23 17:26:55 +0300

    Navbar Fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   });
 
   return (
-    <nav className="navbar container py-6 flex items-center justify-between rounded-b-3xl backdrop-blur">
+    <nav className="navbar container py-4 flex items-center justify-between rounded-b-3xl backdrop-blur">
       <Link href="/" className="flex gap-2 hover:gap-1 justify-center items-center font-medium hover:bg-accent py-2 px-3 rounded-3xl group duration-500 active:rounded-md">
         <AlertOctagon className="group-hover:scale-90 duration-200" />
         Hadith Tech
