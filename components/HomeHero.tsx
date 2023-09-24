e0758ee1aff437a9f2cd95d1467d9457116c48d1
FxRQ9 <mohamadalrifaie2007@gmail.com>
2023-09-24 03:26:20 +0300

    %70 is compeleted

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -1,16 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { buttonVariants } from "./ui/button";
 
 const HomeHero = () => {
   return (
-    <section className="container rounded-3xl h-[75vh] px-4 w-[calc(100vw-32px)] md:h-[75vh] md:min-h-[750px] border-2 max-h-[1200px]">
-      {/* <h1>Hadith Tech</h1>
-      <p>
-        Futuristic podcast with technology leaders Fireside tech <b> podcast</b>{" "}
-        with technology leaders. This podcast hosts people to talk about their
-        experience, most valuable work for them and their vision
-      </p>
-      <div className="flex rounded-md items-center w-fit mx-auto gap-8 h-12 justify-center mt-4">
+    <section className="container rounded-3xl h-[75vh] px-10 w-[calc(100vw-32px)] md:h-[75vh] md:min-h-[750px] border-2 max-h-[1200px] flex flex-row svgbg">
+      {/* squere */}
+      <div className=" relative top-16 left-24  bg-orange-200 h-[400px] rounded w-[370px]   ">
+        <div className="pl-4 pb-2 h-min absolute -left-7 -top-6 rounded white-cafe ml-20 mt-16 flex flex-col gap-7 w-[400px] z-10 ">
+          <h1 className="text-primary title-font  text-6xl font-semibold">
+            Futuristic <span className="redcolor">podcast</span> with technology
+            leaders
+          </h1>
+          <p className="w-96 text-lg font-light">
+            Fireside tech <b className=" "> podcast</b> with technology leaders.
+            This podcast hosts people to talk about their experience, most
+            valuable work for them and their vision
+          </p>
+          <div className="grow-0 w-max">
+            <Link className={buttonVariants()} href="/episodes">
+              start listening
+            </Link>
+            <Link className={buttonVariants()} href="/about">
+              More About hadith
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* photo */}
+      <div><div></div></div>
+
+      {/* <div className="flex rounded-md items-center w-fit mx-auto gap-8 h-12 justify-center mt-4">
         <Image
           alt="google"
           src="/companies_logos/Google.png"
@@ -34,7 +55,6 @@ const HomeHero = () => {
         />
       </div> */}
     </section>
-   
   );
 };
 
