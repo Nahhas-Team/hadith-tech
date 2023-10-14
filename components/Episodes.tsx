f715c48e14a4b308dadeef8741fc733ecafee0a9
Abdulrahman Nahhas <nahhas909@gmail.com>
2023-10-14 11:23:42 +0300

    delete episodes component

--- components/Episodes.tsx
+++ components/Episodes.tsx
@@ -1,15 +1,10 @@
-import Link from 'next/link'
 import React from 'react'
-import { buttonVariants } from './ui/button'
 
 const Episodes = () => {
   return (
-    
-    <section className='pt-20 pb-12 text-5xl font-bold items-center justify-center flex flex-col gap-5'>Episodes
-    <Link className={buttonVariants()} href="/episodes">
-      Go to espisodes
-    </Link>
-    </section>
+    <div>
+      Espisodes
+    </div>
   )
 }
 
