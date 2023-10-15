a80dc641a70c5a2ae8d67cb1726a0b5fb7a7e13b
Abdulrahman Nahhas <nahhas909@gmail.com>
2023-10-15 14:25:47 +0300

    Testimonials + Fix Contact

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,16 +1,13 @@
-import About from '@/components/About'
-import ContactWidget from '@/components/ContactWidget'
-import Episodes from '@/components/Episodes'
+import Testimonials from '@/components/Testimonials'
 import About_HeroSection from '@/components/sections/About/HeroSection'
-import HomeHero from '@/components/sections/Home/HomeHero'
 
 export default function Home() {
   return (
-    <div className='min-h-[calc(100vh-88px-88px-16px)] flex items-center justify-center'>
+    <div className='min-h-[calc(100vh-88px-88px-16px)] flex items-center justify-center flex-col gap-20'>
       <About_HeroSection />
+      <Testimonials />
 
       {/* <Episodes /> */}
-      {/* <About /> */}
       {/* <ContactWidget /> */}
     </div>
   )
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+
+const TestimonialsCard = () => (
+  <div className="rounded-3xl border duration-300 hover:shadow-md p-6">
+    <div className="flex flex-row">
+      <div className="p-2 w-12 h-12 mr-4 bg-primary rounded-full text-white flex items-center justify-center text-xl font-bold">
+     A
+   </div>
+      <div className="flex flex-col gap-0">
+        <h1 className="text-2xl font-medium">
+          Ali Mcharka
+        </h1>
+        <h3 className="text-primary font-bold text-sm">
+          Programmer
+        </h3>
+      </div>
+    </div>
+    <p className="mt-2 pb-1">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad magnam
+      asperiores totam possimus eius quam.
+    </p>
+  </div>
+);
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section className="container my-10">
+      <div className="flex flex-col gap-6 mb-10">
+        <h1 className="text-5xl font-medium">
+          What Our <span className="text-primary font-bold">Clients</span> Say?
+        </h1>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          unde est deleniti... <br /> similique Inventore unde est deleniti,
+          similique
+        </p>
+      </div>
 
-export default Testimonials
+      <div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <TestimonialsCard />
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
